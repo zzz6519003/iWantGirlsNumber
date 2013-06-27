898a14cf56e1b2f392b90e91d3fd5d72f29cf86c
zzz6519003 <zhengzhongzhao@gmail.com>
2013-06-27 14:27:50 +0800

    add vc

---
+++ iWannaGirlsNumber/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  iWannaGirlsNumber
+//
+//  Created by Snowmanzzz on 13-6-27.
+//  Copyright (c) 2013年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
